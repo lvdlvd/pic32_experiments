0d36b7933d64397be9b57c8fbd07a841c75a255a
Luuk van Dijk <lvd@daedalean.ai>
2021-09-19 17:39:17 +0200

    cleaned up ringbuffer

--- printf.h
+++ printf.h
@@ -4,7 +4,7 @@
     The implemenation is done by stb_printf.h
 
     Typically you make a puts_t callback to copy to ringbuffer and enable the irq or dma
-    that empties it, and in the irq/dma handler you pull from teh ringbuffer and switch it off when empty.
+    that empties it, and in the irq/dma handler you pull from the ringbuffer and switch it off when empty.
 */
 
 #include <stdarg.h>  // for varargs
@@ -17,13 +17,16 @@ struct Ringbuffer {
     uint16_t tail;         // reads happen here
 };
 
-inline uint16_t ringbuffer_avail(const struct Ringbuffer *rb) { return rb->head - rb->tail; }                         // 0..size -1
-inline uint16_t ringbuffer_free(const struct Ringbuffer *rb) { return sizeof(rb->buf) - (rb->head - rb->tail) - 1; }  // size-1 .. 0
+// invariant:
+// as long as you only put_head() if !full(), and get_tail() if !empty(), tail <= head and head-tail < sizeof buf.
+
+inline uint16_t ringbuffer_avail(const struct Ringbuffer *rb) { return rb->head - rb->tail; }                          // 0..size -1
+inline uint16_t ringbuffer_free(const struct Ringbuffer *rb)  { return sizeof(rb->buf) - (rb->head - rb->tail) - 1; }  // size-1 .. 0
 inline int      ringbuffer_empty(const struct Ringbuffer *rb) { return rb->head == rb->tail; }
-inline int      ringbuffer_full(const struct Ringbuffer *rb) { return (rb->head - rb->tail) >= (int)(sizeof(rb->buf) - 2); }
-inline void     ringbuffer_clear(struct Ringbuffer *rb) { rb->head = rb->tail; }
-inline void     put_head(struct Ringbuffer *rb, uint8_t c) { rb->buf[rb->head++ & (sizeof(rb->buf) - 1)] = c; }
-inline uint8_t  get_tail(struct Ringbuffer *rb) { return rb->buf[rb->tail++ & (sizeof(rb->buf) - 1)]; }
+inline int      ringbuffer_full(const struct Ringbuffer *rb)  { return sizeof(rb->buf) - 3 < (uint16_t)(rb->head - rb->tail); }
+inline void     ringbuffer_clear(struct Ringbuffer *rb)       { rb->head = rb->tail; } // may race with get_tail
+inline void     put_head(struct Ringbuffer *rb, uint8_t c)    { rb->buf[rb->head++ & (sizeof(rb->buf) - 1)] = c; }
+inline uint8_t  get_tail(struct Ringbuffer *rb)               { return rb->buf[rb->tail++ & (sizeof(rb->buf) - 1)]; }
 
 // copies as much of buf[:len] to rb as will fit, returns the number of bytes copied.
 size_t ringbuffer_puts(struct Ringbuffer *rb, const char *buf, size_t len);
